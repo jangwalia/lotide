95185d1bdeb9f4bbc3fb34d83356e08fbf0cf8fc
jang walia <jangiwalia83@gmail.com>
2021-10-23 05:41:30 -0700

    refactoring files for eslint

--- flatten.js
+++ flatten.js
@@ -1,4 +1,4 @@
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
@@ -7,28 +7,26 @@ const eqArrays = function (arr1, arr2) {
       return false;
     }
   } return true;
-}
-const assertArraysEqual = function (ar1, ar2) {
+};
+const assertArraysEqual = function(ar1, ar2) {
   if (eqArrays(ar1, ar2)) {
     console.log(`😄 Assertion Passed :${ar1} === ${ar2}`);
-  }
-  else {
+  } else {
     console.log(`💀 Assertion Failed :${ar1} !== ${ar2}`);
   }
-}
+};
 
-const flatten = function (arr) {
+const flatten = function(arr) {
   let finalArr = [];
   for (let j = 0; j < arr.length; j++) {
     if (Array.isArray(arr[j])) {
       for (let k = 0; k < arr[j].length; k++) {
         finalArr.push(arr[j][k]);
       }
-    }
-    else {
+    } else {
       finalArr.push(arr[j]);
     }
   } return finalArr;
-}
+};
 console.log(flatten([1, 2, [3, 4], 5, [6]]));
 
--- without.js
+++ without.js
@@ -1,32 +1,31 @@
- const eqArrays = function(arr1,arr2) {
-  if(arr1.length !== arr2.length) {
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
     return false;
   }
-  for(let i = 0;i < arr1.length;i++){
-    if(arr1[i] !== arr2[i]){
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
       return false;
     }
-  }return true;
-}
-const assertArraysEqual = function(ar1,ar2){
-  if(eqArrays(ar1,ar2)){
+  } return true;
+};
+const assertArraysEqual = function(ar1, ar2) {
+  if (eqArrays(ar1, ar2)) {
     console.log(`😄 Assertion Passed :${ar1} === ${ar2}`);
-  }
-  else{
+  } else {
     console.log(`💀 Assertion Failed :${ar1} !== ${ar2}`);
   }
-} 
+};
 
-const without = function(arr,item){
+const without = function(arr, item) {
   let newArr = [];
-  for(let i = 0;i < arr.length;i++) {
-    if(arr[i] !== item[i]){
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== item[i]) {
       newArr.push(arr[i]);
     }
-  }return newArr;
-}
+  } return newArr;
+};
 console.log(without([1, 2, 3], [1]));
 console.log(without(["1", "2", "3"], [1, 2, "3"]));
 const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); 
+without(words, ["lighthouse"]);
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
