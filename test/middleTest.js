f37678aec23d69f2aefd5324a1daa1e5f0eead0b
jang walia <jangiwalia83@gmail.com>
2021-11-10 17:21:33 -0800

    Refactored tail and middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,13 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const eqArrays = require('../eqArray');
+const assert = require('chai').assert;
 const middle = require('../middle');
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([2, 3, 4, 7, 1, 6, 8]), [7]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 4]);
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1, 2]), []);
+describe("#middle",()=>{
+  it("should return [2] for [1,2,3]",()=>{
+    assert.deepEqual(middle([1,2,3]),[2]);
+  });
+  it("should return [3] for [1,2,3,4,5]",()=>{
+    assert.deepEqual(middle([1,2,3,4,5]),[3]);
+  });
+  it("should return [3,4] for [1,2,3,4,5,6]",()=>{
+    assert.deepEqual(middle([1,2,3,4,5,6]),[3,4]);
+  });
+})
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,11 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
+describe('#tail',()=>{
+  it("returns [Lighthouse, Labs] for [Hello, Lighthouse, Labs])",()=>{
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]),["Lighthouse", "Labs"])
+  });
+  it("returns [Labs] for [Lighthouse, Labs]",()=>{
+    assert.deepEqual(tail(["Lighthouse", "Labs"]),["Labs"])
+  });
+ 
+})
