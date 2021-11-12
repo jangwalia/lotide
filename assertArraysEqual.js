d97e23e58a5d44de7a385c340f16b7a0f8354e52
jang walia <jangiwalia83@gmail.com>
2021-11-12 07:54:48 -0800

    commented all previous assert functions

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,10 @@
-const eqArrays = require('./eqArray');
-const assertArraysEqual = function(ar1, ar2) {
-  if (eqArrays(ar1, ar2)) {
-    console.log(`😄 Assertion Passed :${ar1} === ${ar2}`);
-  } else {
-    console.log(`💀 Assertion Failed :${ar1} !== ${ar2}`);
-  }
-};
+// const eqArrays = require('./eqArray');
+// const assertArraysEqual = function(ar1, ar2) {
+//   if (eqArrays(ar1, ar2)) {
+//     console.log(`😄 Assertion Passed :${ar1} === ${ar2}`);
+//   } else {
+//     console.log(`💀 Assertion Failed :${ar1} !== ${ar2}`);
+//   }
+// };
 
-module.exports = assertArraysEqual;
+// module.exports = assertArraysEqual;
--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
-const assertEqual = function(actual,expected) {
-  if (actual === expected) {
-    console.log(`😂 Assertion Passed :${actual} === ${expected}`);
-  } else {
-    console.log(`💀 Assertion Failed :${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual,expected) {
+//   if (actual === expected) {
+//     console.log(`😂 Assertion Passed :${actual} === ${expected}`);
+//   } else {
+//     console.log(`💀 Assertion Failed :${actual} !== ${expected}`);
+//   }
+// };
 
 
-module.exports = assertEqual;
+// module.exports = assertEqual;
